d5cc726c1a3cee6565581ba997e05351e9eb4ac5
gregory <burtelgamerpro@gmail.com>
2024-11-17 15:24:17 +0600

    fix

--- startup.py
+++ startup.py
@@ -6,6 +6,7 @@ import os
 
 class Application:
     def __init__(self):
+<<<<<<< HEAD
         # Загрузка конфигурации
         self.config_loader = ConfigLoader("appsettings.json")
         
@@ -47,6 +48,36 @@ class Application:
         # Инициализация компонентов приложения
         self.downloader = Downloader(yt_dlp_path, download_path)
         self.playlist_controller = PlaylistController(playlist_dir)
+=======
+        # �������� ������������ � ���������
+        self.config_loader = ConfigLoader("appsettings.json")
+        
+        # �������� ����� ��� yt_dlp � ���������� ��������
+        yt_dlp_path = self.config_loader.get("yt_dlp_path", "")
+        download_path = self.config_loader.get("download_path", "")
+        
+        if not yt_dlp_path or not os.path.exists(yt_dlp_path):
+            raise ValueError(f"������������ ���� � yt-dlp: {yt_dlp_path}")
+        
+        if not download_path or not os.path.isdir(download_path):
+            raise ValueError(f"������������ ���� ��� ��������: {download_path}")
+        
+        self.downloader = Downloader(yt_dlp_path, download_path)
+        
+        # �������� ���� � ���������� ���������
+        playlist_dir = self.config_loader.get("playlist_dir", "")
+        if not playlist_dir or not os.path.isdir(playlist_dir):
+            raise ValueError(f"������������ ���� � ���������� ���������: {playlist_dir}")
+        
+        self.playlist_controller = PlaylistController(playlist_dir)
+        
+        # ��������� � ��������� ������ Discord
+        discord_token = self.config_loader.get("discord_token", "")
+        if not discord_token:
+            raise ValueError("����� Discord �� ������ ��� �����������.")
+        
+        # ������������� Discord ����
+>>>>>>> e011704d756d619585810eb50b024b454f47513a
         self.discord_bot = BardDiscordBot(
             token=discord_token,
             config_loader=self.config_loader,
