3bdb7f7e1000a488214896333a8bf3a72b756da8
gregory <burtelgamerpro@gmail.com>
2024-11-20 01:35:05 +0600

    new PlaylistManager

---
+++ MagicBard.PlaylistAPI/PlaylistService.cs
@@ -0,0 +1,229 @@
+﻿using Newtonsoft.Json;
+using System.IO.Abstractions;
+
+namespace MagicBard.PlaylistAPI
+{
+    public class PlaylistService : IPlaylistService, IHostedService
+    {
+        private readonly string PlaylistFolder;
+        private readonly string PlaylistFile;
+        private readonly ILogger<PlaylistService> _logger;
+        private readonly IFileSystem _fileSystem;
+
+        private List<Track> Playlist { get; set; } = new();
+        private List<Track> OriginalOrder { get; set; } = new();
+        private int CurrentIndex { get; set; } = 0;
+        private int RepeatMode { get; set; } = 0;
+
+        private CancellationTokenSource _cancellationTokenSource;
+        private Task _backgroundTask;
+
+        public PlaylistService(IConfiguration configuration, ILogger<PlaylistService> logger, IFileSystem fileSystem)
+        {
+            PlaylistFolder = configuration.GetValue<string>("AppSettings:PlaylistFolder") ?? "/playlist";
+            PlaylistFile = configuration.GetValue<string>("AppSettings:PlaylistFile") ?? "playlist.json";
+            _logger = logger;
+            _fileSystem = fileSystem;
+
+            _logger.LogInformation($"Playlist folder: {PlaylistFolder}");
+
+            LoadTracks();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Playlist service started.");
+
+            _cancellationTokenSource = new CancellationTokenSource();
+            _backgroundTask = Task.Run(() => BackgroundTask(_cancellationTokenSource.Token), cancellationToken);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Playlist service stopping.");
+
+            _cancellationTokenSource.Cancel();
+            return _backgroundTask ?? Task.CompletedTask;
+        }
+
+        private async Task BackgroundTask(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                try
+                {
+                    UpdatePlaylist();
+                    await Task.Delay(TimeSpan.FromSeconds(10), token); // Обновляем плейлист каждые 10 сек
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Error in background task: {ex.Message}");
+                }
+            }
+        }
+
+        public void LoadTracks()
+        {
+            if (_fileSystem.File.Exists(PlaylistFile))
+            {
+                var json = _fileSystem.File.ReadAllText(PlaylistFile);
+                Playlist = JsonConvert.DeserializeObject<List<Track>>(json) ?? new List<Track>();
+                _logger.LogInformation("Playlist loaded from file.");
+            }
+            else
+            {
+                Playlist = _fileSystem.Directory
+                    .EnumerateFiles(PlaylistFolder)
+                    .Where(filePath => IsSupportedFileExtension(filePath))
+                    .Select(filePath => new Track
+                    {
+                        Title = Path.GetFileNameWithoutExtension(filePath),
+                        Path = Path.GetFileName(filePath)
+                    })
+                    .ToList();
+                SavePlaylist();
+                _logger.LogInformation("Playlist created from folder files.");
+            }
+
+            OriginalOrder = new List<Track>(Playlist);
+        }
+
+        private bool IsSupportedFileExtension(string filePath)
+        {
+            var supportedExtensions = new[] { ".mp3", ".wav", ".webm", ".flac" };
+            return supportedExtensions.Contains(Path.GetExtension(filePath).ToLower());
+        }
+
+        public void SavePlaylist()
+        {
+            var json = JsonConvert.SerializeObject(Playlist, Formatting.Indented);
+            _fileSystem.File.WriteAllText(PlaylistFile, json);
+            _logger.LogInformation("Playlist saved to file.");
+        }
+
+        public void UpdatePlaylist()
+        {
+            var folderFiles = new HashSet<string>(
+                _fileSystem.Directory.GetFiles(PlaylistFolder)
+                    .Where(filePath => IsSupportedFileExtension(filePath))
+                    .Select(filePath => Path.GetFileName(filePath))
+            );
+
+            _logger.LogInformation($"Files in folder: {string.Join(", ", folderFiles)}");
+
+            var playlistFiles = new HashSet<string>(Playlist.Select(t => t.Path));
+
+            _logger.LogInformation($"Files in playlist: {string.Join(", ", playlistFiles)}");
+
+            var newFiles = folderFiles.Except(playlistFiles);
+            _logger.LogInformation($"New files to add: {string.Join(", ", newFiles)}");
+
+            foreach (var fileName in newFiles)
+            {
+                var newTrack = new Track
+                {
+                    Title = Path.GetFileNameWithoutExtension(fileName),
+                    Path = fileName
+                };
+                Playlist.Add(newTrack);
+                OriginalOrder.Add(newTrack);
+                _logger.LogInformation($"New track added: {newTrack.Title}");
+            }
+
+            Playlist = Playlist.Where(track => folderFiles.Contains(track.Path)).ToList();
+            OriginalOrder = OriginalOrder.Where(track => folderFiles.Contains(track.Path)).ToList();
+
+            _logger.LogInformation($"Updated playlist: {string.Join(", ", Playlist.Select(t => t.Title))}");
+
+            SavePlaylist();
+        }
+
+        public List<Track> GetPlaylist() => Playlist;
+
+        public Track GetCurrentTrack()
+        {
+            if (!Playlist.Any())
+            {
+                _logger.LogWarning("Playlist is empty.");
+                throw new InvalidOperationException("Playlist is empty.");
+            }
+            return Playlist[CurrentIndex];
+        }
+
+        public Track GetNextTrack()
+        {
+            if (!Playlist.Any())
+            {
+                _logger.LogWarning("Playlist is empty.");
+                throw new InvalidOperationException("Playlist is empty.");
+            }
+
+            if (RepeatMode == 0 && CurrentIndex + 1 == Playlist.Count)
+            {
+                _logger.LogInformation("End of the playlist.");
+
+                CurrentIndex += 1;
+                var LastIndex = (CurrentIndex + 1) % Playlist.Count;
+                return Playlist[LastIndex];
+            }
+            else if(RepeatMode == 0 && CurrentIndex == Playlist.Count)
+            {
+                CurrentIndex = 0;
+                return new Track();
+            }
+
+            if (RepeatMode == 1)
+            {
+                _logger.LogInformation("Repeat current track.");
+                return Playlist[CurrentIndex];
+            }
+
+            CurrentIndex = (CurrentIndex + 1) % Playlist.Count;
+
+            if (RepeatMode == 2 && CurrentIndex == 0)
+            {
+                _logger.LogInformation("Repeat playlist.");
+                CurrentIndex = 0;
+            }
+
+            return Playlist[CurrentIndex];
+        }
+
+        public void ShufflePlaylist(string mode)
+        {
+            if (mode == "t")
+            {
+                Playlist = Playlist.OrderBy(_ => Guid.NewGuid()).ToList();
+                CurrentIndex = 0;
+                _logger.LogInformation("Playlist shuffled randomly.");
+            }
+            else if (mode == "f")
+            {
+                Playlist = new List<Track>(OriginalOrder);
+                CurrentIndex = 0;
+                _logger.LogInformation("Playlist restored to original order.");
+            }
+            else
+            {
+                _logger.LogWarning("Invalid shuffle mode.");
+                throw new ArgumentException("Invalid mode. Use 't' or 'f'");
+            }
+
+            SavePlaylist();
+        }
+
+        public void SetRepeatMode(int mode)
+        {
+            if (mode < 0 || mode > 2)
+            {
+                _logger.LogWarning("Invalid repeat mode.");
+                throw new ArgumentException("Invalid mode. Use '0', '1', or '2'");
+            }
+
+            RepeatMode = mode;
+            _logger.LogInformation($"Repeat mode set to {RepeatMode}");
+        }
+    }
+}
---
+++ MagicBard.PlaylistAPI/Program.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using Microsoft.OpenApi.Models;
+using System.Net;
+using MagicBard.PlaylistAPI;
+using System.IO.Abstractions;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Register services
+        builder.Services.AddSingleton<IFileSystem, FileSystem>();
+        builder.Services.AddSingleton<IPlaylistService, PlaylistService>();
+        builder.Services.AddHostedService<PlaylistService>();
+
+        // Загрузка конфигурации из appsettings.json
+        builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        // Настройка сервисов
+        builder.Services.AddControllers()
+            .AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.Formatting = Formatting.Indented;
+                options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            });
+
+        // Настройки Kestrel
+        builder.WebHost.ConfigureKestrel(options =>
+        {
+            var ipAddress = builder.Configuration.GetValue<string>("AppSettings:IpAddress");
+            var httpPort = builder.Configuration.GetValue<int>("AppSettings:HttpPort");
+
+            if (string.IsNullOrEmpty(ipAddress))
+            {
+                throw new ArgumentNullException("AppSettings:IpAddress", "IP address is not provided in configuration.");
+            }
+
+            // Указываем только один HTTP порт
+            options.Listen(IPAddress.Parse(ipAddress), httpPort); // Настройка только для HTTP
+        });
+
+        // Добавление Swagger
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            // Настройка информации о Swagger
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Playlist API",
+                Version = "v1",
+                Description = "API for managing playlists"
+            });
+        });
+
+        var app = builder.Build();
+
+        // Настройка middleware
+        app.UseRouting(); // Включаем маршрутизацию
+
+        // Swagger
+        app.UseSwagger();  // Генерация документации Swagger
+        app.UseSwaggerUI(c =>
+        {
+            var swaggerAddress = builder.Configuration.GetValue<string>("AppSettings:SwaggerAddress");
+            var swaggerEndpoint = builder.Configuration.GetValue<string>("AppSettings:SwaggerEndpoint");
+
+            c.SwaggerEndpoint(swaggerEndpoint, "Playlist API V1");
+            c.RoutePrefix = swaggerAddress;
+        });
+
+        // Обработка запросов
+        app.MapControllers();
+        app.Run();
+    }
+}
